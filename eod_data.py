dd701e9bbcbf446ce827963666a8ae7acf97e910
Matt Larsen <matt.larsen@connorgp.com>
2019-07-24 12:27:23 -0600

    eod properties

--- eod_data.py
+++ eod_data.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 
 from ticker import Ticker
 
@@ -7,14 +8,14 @@ class EODData:
     def __init__(self):
         self.Ticker = Ticker()
         self._date = None
-        self.open = None
-        self.close = None
-        self.high = None
-        self.low = None
+        self._open = None
+        self._close = None
+        self._high = None
+        self._low = None
         self.vol = None
 
     def __str__(self):
-        return f"{self.ticker} - {self.date} open: {self.open} high: {self.high} low: {self.low} close: {self.close} vol: {self.vol}"
+        return f"{self.ticker} - {self.date} open: {self.opening} high: {self.high} low: {self.low} close: {self.close} vol: {self.vol}"
 
     @property
     def ticker(self):
@@ -34,3 +35,59 @@ class EODData:
             self._date = datetime.strptime(value, '%Y%m%d')
         except Exception as e:
             raise e
+
+    @property
+    def opening(self):
+        return self._open
+
+    @opening.setter
+    def opening(self, value):
+        try:
+            self._open = Decimal(value)
+        except Exception as e:
+            raise e
+
+    @property
+    def high(self):
+        return self._high
+
+    @high.setter
+    def high(self, value):
+        try:
+            self._high = Decimal(value)
+        except Exception as e:
+            raise e
+
+    @property
+    def low(self):
+        return self._low
+
+    @low.setter
+    def low(self, value):
+        try:
+            self._low = Decimal(value)
+        except Exception as e:
+            raise e
+
+    @property
+    def close(self):
+        return self._close
+
+    @close.setter
+    def close(self, value):
+        try:
+            self._close = Decimal(value)
+        except Exception as e:
+            raise e
+
+    @classmethod
+    def new_eod(cls, ticker, date, opening, high, low, close, vol):
+        eod = cls()
+        eod.ticker = ticker
+        eod.date = date
+        eod.opening = opening
+        eod.high = high
+        eod.low = low
+        eod.close = close
+        eod.vol = vol
+        return eod
--- test_eod_data.py
+++ test_eod_data.py
@@ -4,6 +4,7 @@ from decimal import *
 import pytest
 
 from eod_data import EODData
+from ticker import Ticker
 
 ticker_test_values = [
     ('abc', '20190101', '12.34', '13.45', '11.45', '12.45', 100000),
@@ -17,9 +18,9 @@ def test_eod_ticker_str_value(ticker, date, opening, high, low, close, vol):
     eod.ticker = ticker
     eod.date = date
     eod.open = opening
-    eod.high = high
-    eod.low = low
-    eod.close = close
+    eod._high = high
+    eod._low = low
+    eod._close = close
     eod.vol = vol
     assert eod.ticker == ticker
 
@@ -36,3 +37,64 @@ def test_eod_date_is_datetime(ticker, date, opening, high, low, close, vol):
     eod = EODData()
     eod.date = date
     assert isinstance(eod.date, datetime)
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_eod_opening_is_decimal(ticker, date, opening, high, low, close, vol):
+    eod = EODData()
+    eod.opening = opening
+    assert isinstance(eod.opening, Decimal)
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_eod_opening_decimals_equal(ticker, date, opening, high, low, close, vol):
+    eod = EODData()
+    eod.opening = opening
+    open_decimal = Decimal(opening)
+    assert open_decimal == eod.opening
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_eod_high_is_decimal_and_equal(ticker, date, opening, high, low, close, vol):
+    eod = EODData()
+    eod.high = high
+    high_decimal = Decimal(high)
+    assert isinstance(eod.high, Decimal)
+    assert high_decimal == eod.high
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_eod_low_is_decimal_and_equal(ticker, date, opening, high, low, close, vol):
+    eod = EODData()
+    eod.low = low
+    low_decimal = Decimal(low)
+    assert isinstance(eod.low, Decimal)
+    assert low_decimal == eod.low
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_eod_close_is_decimal_and_equal(ticker, date, opening, high, low, close, vol):
+    eod = EODData()
+    eod.close = close
+    close_decimal = Decimal(close)
+    assert isinstance(eod.close, Decimal)
+    assert close_decimal == eod.close
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_new_eod_Ticker_is_Ticker(ticker, date, opening, high, low, close, vol):
+    eod = EODData.new_eod(ticker, date, opening, high, low, close, vol)
+    assert isinstance(eod.Ticker, Ticker)
+    assert eod.Ticker.ticker == ticker
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_new_eod_close_is_Decimal(ticker, date, opening, high, low, close, vol):
+    eod = EODData.new_eod(ticker, date, opening, high, low, close, vol)
+    assert isinstance(eod.close, Decimal)
+
+
+@pytest.mark.parametrize('ticker, date, opening, high, low, close, vol', ticker_test_values)
+def test_new_eod_close_value_equal_decimal(ticker, date, opening, high, low, close, vol):
+    eod = EODData.new_eod(ticker, date, opening, high, low, close, vol)
+    assert eod.close == Decimal(close)
