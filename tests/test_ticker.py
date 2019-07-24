c51e15064cc8564d186f1d987709902ead39e131
Matt Larsen <matt.larsen@connorgp.com>
2019-07-24 15:35:29 -0600

    gather unique list of Ticker objects from CSV data

--- tests/test_ticker.py
+++ tests/test_ticker.py
@@ -1,4 +1,24 @@
+import io
+
+from ticker.fetch import fetch_tickers_unique
 from ticker.ticker import Ticker
+
+csv_content_string = """<ticker>,<date>,<open>,<high>,<low>,<close>,<vol>
+A,20190722,68.91,69.465,68.46,69.09,3103300
+AA,20190722,23.18,23.3901,22.695,22.98,3719200
+AAC,20190722,0.97,1.02,0.88,0.9099,127600
+AAN,20190722,63.42,64.52,62.98,63.03,563500
+AAP,20190722,157.28,160.17,157.24,158.9,840700
+"""
+
+csv_string_with_duplicates = """<ticker>,<date>,<open>,<high>,<low>,<close>,<vol>
+A,20190722,68.91,69.465,68.46,69.09,3103300
+AA,20190722,23.18,23.3901,22.695,22.98,3719200
+AAC,20190722,0.97,1.02,0.88,0.9099,127600
+AAN,20190722,63.42,64.52,62.98,63.03,563500
+AAP,20190722,157.28,160.17,157.24,158.9,840700
+AAN,20190722,63.42,64.52,62.98,63.03,563500
+"""
 
 
 def test_ticker_str_value():
@@ -9,3 +29,15 @@ def test_ticker_str_value():
 def test_ticker_str_no_name():
     ticker = Ticker(ticker="ABC")
     assert ticker.__str__() == "ABC"
+
+
+def test_tickers_from_csv():
+    f = io.StringIO(csv_content_string)
+    tickers = fetch_tickers_unique(f)
+    assert any([i for i in tickers if i.ticker == "AAN"])
+
+
+def test_tickers_from_csv_with_duplicates():
+    f = io.StringIO(csv_string_with_duplicates)
+    tickers = fetch_tickers_unique(f)
+    assert len([i for i in tickers if i.ticker == "AAN"]) == 1
--- ticker/fetch.py
+++ ticker/fetch.py
@@ -1,2 +1,22 @@
+import csv
+
 from .ticker import Ticker
 
+
+def fetch_tickers_unique(f):
+    """ Given a CSV object return a set of Ticker() objects from ticker column
+
+    :param f:
+    :return:
+    """
+    ticker_objects = []
+    all_ticker_strs = []
+    reader = csv.DictReader(f)
+    for row in reader:
+        all_ticker_strs.append(row['<ticker>'])
+    tickers_set = set(all_ticker_strs)
+    for t in tickers_set:
+        ticker = Ticker()
+        ticker.ticker = t
+        ticker_objects.append(ticker)
+    return ticker_objects
