a6733a060a65ef177c1d2610b77fb9df8c3e784d
Matt Larsen <matt.larsen@connorgp.com>
2019-07-30 17:05:52 -0600

    switch to Numeric in models and load_dotenv() in __init__

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, DECIMAL, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, DECIMAL, ForeignKey, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -16,9 +16,9 @@ class Price(Base):
     id = Column(Integer, primary_key=True, )
     ticker_id = Column(Integer, ForeignKey('tickers.id'), nullable=False)
     date = Column(Date, nullable=False, )
-    open = Column(DECIMAL, nullable=False)
-    high = Column(DECIMAL, nullable=False)
-    low = Column(DECIMAL, nullable=False)
-    close = Column(DECIMAL, nullable=False)
-    adj_close = Column(DECIMAL, nullable=False)
+    open = Column(Numeric, nullable=False)
+    high = Column(Numeric, nullable=False)
+    low = Column(Numeric, nullable=False)
+    close = Column(Numeric, nullable=False)
+    adj_close = Column(Numeric, nullable=False)
     volume = Column(Integer, nullable=False)
--- database/settings.py
+++ database/settings.py
@@ -1,7 +1,3 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 DB_STRING = os.getenv("DB_STRING")
