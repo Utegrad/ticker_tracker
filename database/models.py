dc2d80971ea0523ce627363f437dfdf32506dec5
Matt Larsen <matt.larsen@connorgp.com>
2019-07-31 10:27:41 -0600

    basic engine and session

--- database/models.py
+++ database/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, DECIMAL, ForeignKey, Numeric
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -10,11 +11,18 @@ class Ticker(Base):
     name = Column(String(length=64), nullable=True)
     ticker = Column(String(length=8), nullable=False, index=True, unique=True)
 
+    prices = relationship("Price", back_populates="ticker")
+
+    def __repr__(self):
+        return f"<Ticker('{self.ticker}')>"
+
+    def __str__(self):        return f"{self.ticker}"
+
 
 class Price(Base):
     __tablename__ = "prices"
     id = Column(Integer, primary_key=True)
-    ticker_id = Column(Integer, ForeignKey("tickers.id"), nullable=False)
+    ticker_id = Column(Integer, ForeignKey('tickers.id'), nullable=False)
     date = Column(Date, nullable=False)
     open = Column(Numeric, nullable=False)
     high = Column(Numeric, nullable=False)
@@ -22,3 +30,8 @@ class Price(Base):
     close = Column(Numeric, nullable=False)
     adj_close = Column(Numeric, nullable=False)
     volume = Column(Integer, nullable=False)
+
+    ticker = relationship("Ticker", back_populates="prices")
+
+    def __repr__(self):
+        return f"<Price('{self.ticker}')"
--- load_database.py
+++ load_database.py
@@ -9,20 +9,35 @@ from database import settings as db_settings
 TICKER_DOWNLOADS = os.getenv("TICKER_DOWNLOADS")
 
 
+def get_session(engine=None, echo=None):
+    """ Return a sqlalchemy Session()
+
+    :param engine:  Defaults to creating a new session if engine is None
+    :param echo: Defaults to False, used to set echo parameter if creating an engine
+    :return: sqlalchemy session
+    """
+
+    echo = False if echo is None else echo
+
+    if engine is None:
+        engine = create_engine(db_settings.DB_STRING, echo=echo)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
 def load_database():
+
     engine = create_engine(db_settings.DB_STRING, echo=True)
     base = Base
     base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = get_session(engine=engine)
 
-    a_ticker = Ticker(ticker="A")
-    session.add(a_ticker)
-
-    the_ticker = session.query(Ticker).filter_by(ticker='A').first()
-    print(the_ticker)
-    session.commit()
+    #  Get a list of csv files from the TICKER_DOWNLOADS folder
+    #  For each CSV file create a Ticker object in the database
+    #  For each CSV file create Price records for every record in the CSV file associated to that files Ticker()
 
 
 if __name__ == "__main__":
